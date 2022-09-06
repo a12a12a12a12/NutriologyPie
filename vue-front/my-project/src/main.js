ae070055edc5336bb072cf8a4b608bce22b466ff
WENQIANG WANG <wenqiang.wang2@uqconnect.edu.au>
2022-09-07 02:15:48 +1000

    install elementUI

--- vue-front/my-project/src/main.js
+++ vue-front/my-project/src/main.js
@@ -3,5 +3,7 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router/router.js";
 import store from "./store/state.js";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(router).use(store).use(ElementPlus).mount("#app");
