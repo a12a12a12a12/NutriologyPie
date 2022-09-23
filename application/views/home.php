35c27e19c663fbcc173d862fe8a0a14b276a8f81
WENQIANG WANG <wenqiang.wang2@uqconnect.edu.au>
2022-09-24 01:28:00 +1000

    home page view

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,79 @@
-<div class="container">
-    <div class="bg-[#E3D5BA]">
-
+<div class="home">
+    <div class="home-top bg-[#E3D5BA]">
+        <div class="home-text-top text-4xl">
+            <span class="text-[#2C8F5A]">Learn</span> more to <span>protect</span>you and your family
+        </div>
+        <div class="home-text-middle">
+            Learn more to protect you and your family
+        </div>
+        <div class="home-text-last">
+            Learn more to protect you and your family
+        </div>
+        <div class="home-button">
+            Learn more to protect you and your family
+        </div>
+    </div>
+    <div class="home-bottom">
+        <div class="header">
+            <img class="h-30 w-96 pb-2" src="<?php echo base_url(); ?>assets/images/home1.png" alt="quick start">
+        </div>
+        <div class="quick-start-icon">
+            <div class="icon">
+                <a href="#"><img class="h-30 w-96 pb-2" src="<?php echo base_url(); ?>assets/images/home2.png"
+                        alt="quick start"></a>
+                <h2 class="text-[#6F6D6D] text-center text-3xl font-extrabold">Nutrition Topics</h2>
+            </div>
+            <div class="icon">
+                <a href="#"><img class="h-30 w-96 pb-2" src="<?php echo base_url(); ?>assets/images/home3.png"
+                        alt="quick start"></a>
+                <h2 class="text-[#6F6D6D] text-center text-3xl font-extrabold">Source Websites</h2>
+            </div>
+            <div class="icon">
+                <a href="#"><img class="h-30 w-96 pb-2" src="<?php echo base_url(); ?>assets/images/home4.png"
+                        alt="quick start"></a>
+                <h2 class="text-[#6F6D6D] text-center text-3xl font-extrabold">Experts Online</h2>
+            </div>
+        </div>
     </div>
 </div>
+
+
+
+
+
+<style>
+.home .home-top {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    align-content: center;
+    width: 100%;
+    height: 45%;
+    position: relative;
+}
+
+.home .home-top .home-text-top {
+    position: absolute;
+    width: 20em;
+    height: 10em;
+    left: 5em;
+    top: 6em;
+    line-height: 2em;
+    color: #000000;
+}
+
+.home .home-bottom .quick-start-icon {
+    padding-bottom: 5em;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    align-content: center;
+    width: 100%;
+}
+
+.home .home-bottom .quick-start-icon .icon {
+    width: 20%;
+}
+</style>
