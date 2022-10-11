ed3bc7fa8c1e9843de4413357758602f81687b5d
WENQIANG WANG <wenqiang.wang2@uqconnect.edu.au>
2022-10-12 00:46:21 +1000

    updating expert extension

--- application/controllers/Extension_expert.php
+++ application/controllers/Extension_expert.php
@@ -11,24 +11,25 @@ class Extension_expert extends CI_Controller {
 		$this->load->view('extension_expert');
 	}
 
+	// 爬虫入口
 	public function getUrl() {
 		return "888";
 	}
 
+	//save the comment
 	public function testing()
 	{	
-
-		$data['error']= "";
-		$this->load->helper('form');
-		$this->load->helper('url');
-		// $this->load->view('components/header');
-		// $this->load->view('extension_expert');
-		$testing = $this->input->get('category');
-		echo "999";
-		echo $testing;
+		$category = $this->input->get('category');
+		$comment = $this->input->get('content');
+		$rate = $this->input->get('rate');
 		$abc = $this->getUrl();
-		echo $abc;
+	}
 
+	//number of click add++
+	public function clickIncrease()
+	{	
+		//判断当前url是否存在在article表如果存在click++
+		echo "click++";
 	}
 }
 ?>
